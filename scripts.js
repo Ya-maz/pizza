71cde58af210dc52ee739859e95e0c75177ee0ba
Almaz Yakhin <vochko50@gmail.com>
2021-07-26 13:51:02 +0300

    макет меню

--- scripts.js
+++ scripts.js
@@ -4,3 +4,28 @@
 // div.innerHTML = "<strong>Pizza site!</strong>on development stage";
 
 // document.body.append(div)
+
+function rotateWord(value) {
+    const menuInner = document.querySelectorAll(".menuCircle_inner");
+    
+    deg = -65;
+    for (let item of menuInner){
+        item.style.transform = `rotate(${deg}deg)`;
+        deg += value;
+    }
+
+    return value;
+}
+rotateWord(2);
+
+function rotateSymbol(value) {
+    const menuContent = document.querySelectorAll(".menuCircle_content");
+    
+    deg = 0;
+    for (let item of menuContent){
+        item.style.transform = `rotate(${deg}deg)`;
+        deg += value;
+    }
+}
+
+rotateSymbol(1.5)
