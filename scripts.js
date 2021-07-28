14d1477227ccc160432a2652f37d67c98e785c5d
Almaz Yakhin <vochko50@gmail.com>
2021-07-28 16:10:41 +0300

    size change works

--- scripts.js
+++ scripts.js
@@ -20,5 +20,35 @@ function rotateSymbol(value) {
         deg += value;
     }
 }
-
 rotateSymbol(1.5)
+
+function checkSize () {
+    const size = document.querySelectorAll("input"),
+          img = document.querySelector("img")
+    for (let item of size){
+        if (item.checked) {
+            switch (item.value.toString()) {
+                case 'S':
+                    img.style.transform = "scale(0.9)";
+                break;
+                case 'M':
+                    img.style.transform = "scale(1.1)";
+                break;
+                case 'L':
+                    img.style.transform = "scale(1.4)";
+                break;
+            }
+            console.log(item)
+
+        }
+    }
+}
+checkSize()
+
+function changeSize() {
+    const choiseSize = document.querySelectorAll(".choiseSize")
+    for (let item of choiseSize) {
+        item.addEventListener('click', checkSize)
+    }
+}
+window.addEventListener('click', changeSize())
